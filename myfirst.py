1691304e942693911121d8caac4e26b44632fe91
gururamu08 <gururamu08@gmail.com>
2023-01-26 16:39:58 +0000

    updated

---
+++ myfirst.py
@@ -0,0 +1,24 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+content ="""
+<html>
+<head>
+</head>
+<body>
+<h1>DJANGO</h1>
+</body>
+</html>
+"""
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+
+server_adress =('',80)
+httpd =HTTPServer(server_adress, HelloHandler)
+print("my webserver is running...")
+httpd.serve_forever()
